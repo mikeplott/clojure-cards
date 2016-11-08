6724f058504b0e413b6b405536ad41cabe496f1f
michaelplott <mike.plott80@gmail.com>
2016-11-08 13:50:24 -0500

    I feel like all of these should work.

--- src/clojure_cards/core.clj
+++ src/clojure_cards/core.clj
@@ -32,12 +32,60 @@
 
 (defn flush? [hand]
   (= 1 (count (set (map :suit hand)))))
+
+;(defn straight-flush? [hand]
+ ; (= 1 (count (set (map :suit hand)))
+  ;   (sequential? (set (map :rank hand)))))
+
+(defn straight-flush? [hand]
+  (seq? (seq hand)))
+
+;(defn straight-flush? [hand]
+ ; (= 1 (count (set (map :suit hand)))
+  ;   (seq? (seq (map :rank hand)))))
+
+;(defn straight-flush? [hand]
+ ; (sequential? hand))
+
+;(defn straight? [hand]
+ ; (= 4 (count (set (map :rank hand))))
+  ;(sequential? hand))
+
+(defn straight? [hand]
+  (= 4 (count (set (map :rank hand))))
+  (seq? (seq hand)))
+
+(defn four-of-a-kind? [hand]
+  (= 1 (count (set (map :rank hand)))))
+
+(defn three-of-a-kind? [hand]
+  (= 2 (count (set (map :rank hand)))))
+
+;(defn straight-flush? [hand]
+ ; (flush? hand
+  ;  (sequential? hand)))
+
+;(defn straight-flush? [hand]
+ ; (if flush? [hand]
+  ;  (= 4 (count (set (map :rank hand)))
+   ;    (sequential? hand)))
+
+
+;(defn straight-flush? [hand]
+ ; (= 1 (count (set (map :suit hand)))
+  ;   (= 4 (count (set (map :rank hand))))
+   ;  (sequential? hand)))
+    
   
 
 (defn -main [& args]
   (let [deck (create-deck)
         hands (create-hands deck)
-        hands (filter flush? hands)]
+        ;hands (filter flush? hands)
+        ;hands (filter straight-flush? hands)]
+        ;hands (filter straight? hands)]
+        ;hands (filter four-of-a-kind? hands)]
+        hands (filter three-of-a-kind? hands)]
     (println (count hands))))
   
 
